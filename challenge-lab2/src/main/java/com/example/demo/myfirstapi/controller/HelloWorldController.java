ae88c5c7546e542bf7ca3484e38dd8b921063a36
jovito.isorena <jovito.isorena.jr@gmail.com>
2025-07-16 16:03:54 +0800

    done with challenge lab 2

--- challenge-lab2/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ challenge-lab2/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -35,18 +35,4 @@ public class HelloWorldController {
         int sum = number1 + number2;
         return "The sum of " + number1 + " and " + number2 + " is " + sum;
     }
-
-    @GetMapping("/info")
-    public Map<String,String> getAppInfo(){
-        Map<String, String> info = new HashMap<>();
-        info.put("appName", "MyFirstSpringBootAPI");
-        info.put("version","1.0.0");
-        info.put("status","Running");
-        return info;
-    }
-
-    @GetMapping("/features")
-    public List<String> getFeatures(){
-        return Arrays.asList("REST API", "Spring Boot", "Easy Setup", "Fast Development");
-    }
 }
